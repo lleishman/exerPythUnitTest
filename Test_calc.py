6f8f762cce79026331a0a5c441f64cebb28df24c
L3 <lana.leishman@gmail.com>
2022-10-10 12:10:03 -0600

    commit

--- Test_calc.py
+++ Test_calc.py
@@ -4,5 +4,16 @@ import calc
 class TestCalc(unittest.TestCase):   
    
     def test_add(self):
-        result = calc.add(10, 5)
-        self.assertEqual(result, 15)
+        self.assertEqual(calc.add(10, 5), 15)
+        self.assertEqual(calc.add(-1, 1), 0)
+        self.assertEqual(calc.add(-1, -1), -2)
+        
+if __name__ == '__main__':
+    unittest.main()
+    
+    
+    
+    
+
+
+    
